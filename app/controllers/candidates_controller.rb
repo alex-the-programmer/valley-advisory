3d13fd93256f4269bc48e9c75a80cfec41216b23
Alex Marchenko <AlexTheProgrammerMail@gmail.com>
2017-02-18 20:27:54 -0800

    Scaffolded views and controller updated

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -64,7 +64,7 @@ class CandidatesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_candidate
-      @candidate = Candidate.find(params[:id])
+      @candidate = Candidate.include(:user).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
