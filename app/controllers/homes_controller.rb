ee2bf1b8857349b7b997e6a7d7d6fc23590b553f
Alex Marchenko <AlexTheProgrammerMail@gmail.com>
2017-02-18 18:03:21 -0800

    Stubbed homepages added to advisors, applicants and hiring managers

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,12 @@
 class HomesController < ApplicationController
   def index
+    view = case current_user&.role
+             when nil then 'signed_out_home'
+             when 'applicant' then 'applicant_home'
+             when 'hiring_manager' then 'hiring_manager_home'
+             when 'advisor' then 'advisor home'
+           end
+
+    render view
   end
 end
